d45bff6fd4e1aa42af5bee6c060a6344ed2979d4
y0shiki <yoshikiyarlagadda@gmail.com>
2023-10-10 20:27:28 -0700

    Upload Yoshiki's assignment

---
+++ Project Documents/Assignments/Yoshiki/Peace/Peace.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Peace.Logging;
+
+namespace Peace.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Log a statement to the databse: ");
+
+            string log = Console.ReadLine();
+
+            LogData logData = new LogData();
+
+            logData.Log(log);
+
+            Console.WriteLine("Your statement has been logged.");
+
+        }
+    }
+}
---
+++ Project Documents/Assignments/Yoshiki/Peace/Peace.Logging/Logging.cs
@@ -0,0 +1,21 @@
+﻿namespace Peace.Logging
+{
+    public class LogData
+    {
+        DateTime currentDateTime = DateTime.Now;
+        Peace.DataAccess.DataAccess dataAccess;
+
+        public LogData()
+        {
+            string connectionString = "Server=LAPTOP-JNHE03DV;Database=DemoDB;User Id=Yoshiki;Password=Yoshiki@0103;";
+            this.dataAccess = new Peace.DataAccess.DataAccess(connectionString);
+        }
+
+        public void Log(string userInput)
+        {
+            this.dataAccess.SaveDataToTable(userInput, currentDateTime);
+        }
+
+
+    }
+}
