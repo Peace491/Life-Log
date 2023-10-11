7d9ab628ff7a9c1300a1d28a5598228524d68a3a
Devin2502 <devinkothari25@gmail.com>
2023-10-11 16:39:06 -0700

    Upload Assignment

---
+++ Project Documents/Assignments/Devin/Lifelog.Logging/Peace.Logging/Logging.cs
@@ -0,0 +1,21 @@
+﻿namespace Peace.Logging
+{
+    public class LogData
+    {
+        DateTime currentDateTime = DateTime.Now;
+        Peace.Logging.DataAccess.DataAccess dataAccess;
+
+        public LogData()
+        {
+            string connectionString = "Server=LAPTOP-IDD37K5P;Database=491;User Id=devin;Password=kothari;";
+            this.dataAccess = new Peace.Logging.DataAccess.DataAccess(connectionString);
+        }
+
+        public void Log(string userInput)
+        {
+            this.dataAccess.SaveDataToTable(userInput, currentDateTime);
+        }
+
+
+    }
+}
