500763ffa449d116c595f91a4ff6ecfa09ee7ead
Zarifzz <mylegendaries75@gmail.com>
2023-10-11 16:53:04 -0700

    upload Assignment 1

---
+++ Project Documents/Assignments/Zarif/Peace.Lifelog/Peace.Logging/Logging.cs
@@ -0,0 +1,21 @@
+﻿namespace Peace.Logging
+{
+    public class LogData
+    {
+        DateTime currentDateTime = DateTime.Now;
+        Peace.Logging.DataAccess.DataAccess dataAccess;
+
+        public LogData()
+        {
+            string connectionString = "Server=DESKTOP-0VURBLE;Database=TestDB;User Id=Zarif;Password=Zarif123;";
+            this.dataAccess = new Peace.Logging.DataAccess.DataAccess(connectionString);
+        }
+
+        public void Log(string userInput)
+        {
+            this.dataAccess.SaveDataToTable(userInput, currentDateTime);
+        }
+
+
+    }
+}
