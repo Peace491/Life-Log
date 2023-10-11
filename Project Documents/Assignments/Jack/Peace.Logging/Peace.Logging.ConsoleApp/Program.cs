8068ccca6d8e29f50d01fb921d5b714bab67e7ce
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2023-10-11 15:37:52 -0700

    jack assignmnet submission

---
+++ Project Documents/Assignments/Jack/Peace.Logging/Peace.Logging.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Peace.Logging.ConsoleApp
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Import the logger and use its newlog method to log
+            Logger logger = new Logger();
+            logger.NewLog();
+        }
+    }
+}
---
+++ Project Documents/Assignments/Jack/Peace.Logging/Peace.Logging.DataAccess/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿namespace Peace.Logging.DataAccess;
+
+using System;
+using MySql.Data.MySqlClient;
+
+public class DataAccess
+{
+    // Data Access responsability is to access datastore and insert/save info
+    // In this case log info
+    public void SaveLog(DateTime timeStamp, string logMessage)
+    {
+        // connection string to connect
+        string connectionString = "Server=localhost;Database=Peace;Uid=root;Pwd=JackPickle123;";
+
+        using MySqlConnection connection = new MySqlConnection(connectionString);
+        connection.Open();
+
+        // log insertion query
+        string insertQuery = "INSERT INTO logs (log_timestamp, log_message) VALUES (@timeStamp, @logMessage)";
+
+        using MySqlCommand command = new MySqlCommand(insertQuery, connection);
+        command.Parameters.AddWithValue("@timeStamp", timeStamp);
+        command.Parameters.AddWithValue("@logMessage", logMessage);
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
+}
