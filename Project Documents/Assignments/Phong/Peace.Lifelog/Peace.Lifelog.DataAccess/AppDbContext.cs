321f495f1fb1ce29dc011cb330da8da172a9446c
Phong Cao <phongcao0714@gmail.com>
2023-10-11 14:44:41 -0700

    Add Logging Assignment

---
+++ Project Documents/Assignments/Phong/Peace.Lifelog/Peace.Lifelog.DataAccess/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Peace.Lifelog.DataAccess
+{
+    /// <summary>
+    /// Represents a database context for managing database connections.
+    /// </summary>
+    public class AppDbContext
+    {
+        /// <summary>
+        /// Gets or sets the connection string for the database.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppDbContext"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string for the database.</param>
+        public AppDbContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Creates and returns a new instance of MySqlConnection using the stored connection string.
+        /// </summary>
+        /// <returns>An instance of MySqlConnection.</returns>
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(this.ConnectionString);
+        }
+    }
+}
---
+++ Project Documents/Assignments/Phong/Peace.Lifelog/Peace.Lifelog.TestConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using Peace.Lifelog.Logging;
+
+/// <summary>
+/// Represents the entry point of the application.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// The main method of the application.
+    /// </summary>
+    /// <param name="args">Command-line arguments.</param>
+    static void Main(string[] args)
+    {
+        // Connection String to the database
+        string connectionString = "Server = localhost; Database = LifelogDB; User ID = LifelogWriteUser; Password = password;";
+
+        // Create a logger instance
+        Logger logger = new Logger(connectionString);
+
+        // Prompt user for log input
+        Console.WriteLine("Enter log:");
+        string log = Console.ReadLine();
+
+        // Log the entered message
+        logger.Log(log);
+
+        // Indicate successful logging
+        Console.WriteLine("Log Successful");
+    }
+}
