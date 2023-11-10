6abe813d578f9fe4bd141df497b20e1f50a67b39
Phong Cao <phongcao0714@gmail.com>
2023-11-09 19:41:09 -0800

    Add interfaces for sqlDAO

--- Lifelog/Peace.Lifelog.DataAccess/ISqlDAO.cs
+++ Lifelog/Peace.Lifelog.DataAccess/ISqlDAO.cs
@@ -1,6 +1,19 @@
-﻿namespace Peace.Lifelog.DataAccess;
+﻿using DomainModels;
 
-public class Class1
-{
+namespace Peace.Lifelog.DataAccess;
 
+public interface ICreateOnlyDAO {
+    Response CreateData(string sql);
+}
+
+public interface IReadOnlyDAO {
+    Response ReadData(string sql, int count = 10, int currentPage = 0);
+}
+
+public interface IUpdateOnlyDAO {
+    Response UpdateData(string sql);
+}
+
+public interface IDeleteOnlyDAO {
+    Response DeleteData(string sql);
 }
