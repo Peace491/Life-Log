d3f15e916aa163c7589989ab67886799599bd2e9
Phong Cao <phongcao0714@gmail.com>
2023-11-09 22:52:44 -0800

    Add Sprint4 work

---
+++ Lifelog/Peace.Lifelog.DataAccess/SqlDAO.cs
@@ -0,0 +1,11 @@
+using DomainModels;
+using Peace.Lifelog.DataAccess;
+
+public class CreateOnlyDAO : ICreateOnlyDAO 
+{
+    private const string connectionString = ""; 
+
+    Response CreateData(string sql) {
+        throw new NotImplementedException();
+    }
+}
