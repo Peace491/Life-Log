7ba867c5fc63c12844b4d9a45c134804399596a6
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2023-11-21 20:57:02 -0800

    Laid grounds for Logging Tests

--- Lifelog/Peace.Lifelog.LogServiceTest/LoggingShould.cs
+++ Lifelog/Peace.Lifelog.LogServiceTest/LoggingShould.cs
@@ -7,18 +7,78 @@ using Peace.Lifelog.Logging;
 using System.Diagnostics;
 public class LoggingShould
 {
+
+    // Level Testing
+
+    [Fact]
+    public void LoggingShould_CreateAnInfoLog()
+    {
+        // TODO implement
+    }
     [Fact]
     public void LoggingShould_CreateADebugLog()
     {
         // TODO implement
     }
-    // TODO implement above test for all valid log levels
+    [Fact]
+    public void LoggingShould_CreateAWarningLog()
+    {
+        // TODO implement
+    }
+    [Fact]
+    public void LoggingShould_CreateAnErrorLog()
+    {
+        // TODO implement
+    }
+    [Fact]
+    public void LoggingShouldNot_CreateALogWithInvalidLevel()
+    {
+        // TODO implement 
+    }
+
+    // Category Testing
 
     [Fact]
     public void LoggingShould_CreateAViewLog()
     {
         // TODO implement
     }
-    // TODO implement above test for all valid categories
+    [Fact]
+    public void LoggingShould_CreateABuisnessLog()
+    {
+        // TODO implement
+    }
+    [Fact]
+    public void LoggingShould_CreateAServerLog()
+    {
+        // TODO implement
+    }
+    [Fact]
+    public void LoggingShould_CreateADataLog()
+    {
+        // TODO implement
+    }
+    [Fact]
+    public void LoggingShould_CreateAPersistentDataStoreLog()
+    {
+        // TODO implement
+    }
+    [Fact]
+    public void LoggingShouldNot_CreateALogWithInvalidCategory()
+    {
+        // TODO implement 
+    }
 
+    // Message Testing
+
+    [Fact]
+    public void LoggingShould_CreateALogWithAValidMessage()
+    {
+        // TODO implement
+    }
+    [Fact]
+    public void LoggingShouldNot_CreateALogWithAnInvalidMessage()
+    {
+        // TODO implement
+    }
 }
