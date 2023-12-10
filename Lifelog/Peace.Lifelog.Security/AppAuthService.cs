ca513e08a25179e90d40c2aa31a5e4687ba23b53
Phong Cao <phongcao0714@gmail.com>
2023-12-10 11:09:08 -0800

    Implement authorization

--- Lifelog/Peace.Lifelog.Security/AppAuthService.cs
+++ Lifelog/Peace.Lifelog.Security/AppAuthService.cs
@@ -8,8 +8,32 @@ public class AppAuthService
 
     }
 
+    public bool IsAuthorize(AppPrincipal currentPrincipal, 
+                            IDictionary<string, string> requiredClaims) 
+    {
+        // Dictionary<string, string>() {new ("RoleName", "Admin")}
+        // key - RoleName, value - Admin
+        if (currentPrincipal is null) 
+        {
+            throw new ArgumentNullException(nameof(currentPrincipal));
+        }
 
+        if (requiredClaims is null) 
+        {
+            throw new ArgumentNullException(nameof(requiredClaims));
+        }
 
+        foreach (var claim in requiredClaims)
+        {
+            if (!currentPrincipal.Claims.Contains(claim)) 
+            {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
 
 }
 
