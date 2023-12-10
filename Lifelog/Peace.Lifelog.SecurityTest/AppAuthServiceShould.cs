3766fb56b1969e1f85289519e88532c9b5a49ab2
Phong Cao <phongcao0714@gmail.com>
2023-12-10 11:08:56 -0800

    Implement test for authorization

--- Lifelog/Peace.Lifelog.SecurityTest/AppAuthServiceShould.cs
+++ Lifelog/Peace.Lifelog.SecurityTest/AppAuthServiceShould.cs
@@ -1,5 +1,9 @@
 namespace Peace.Lifelog.SecurityTest;
+using Peace.Lifelog.Security;
 using Peace.Lifelog.DataAccess;
+
+using System.Diagnostics;
+
 public class AppAuthServiceShould
 {
     private const int MAX_EXECUTION_TIME_IN_SECONDS = 3;
@@ -30,6 +34,132 @@ public class AppAuthServiceShould
 
         await DDLTransactionDAO.ExecuteDDLCommand(deleteMockTableSql);
     }
+
+    // Authentication Tests
+    // TODO
+
+    // Authorization Tests
+    [Fact]
+    public async void AppAuthServiceShould_ReturnTrue_IfUserIsAuthorize()
+    {
+        // Arrange
+        var timer = new Stopwatch();
+
+        var appAuthService = new AppAuthService();
+  
+        var currentPrincipal = new AppPrincipal();
+        currentPrincipal.UserId = "1";
+        currentPrincipal.Claims = new Dictionary<string, string>() {
+            {"RoleName", "Admin"}
+        };
+
+        var requiredClaims = new Dictionary<string, string>() {
+            {"RoleName", "Admin"}
+        };
+
+
+        // Act
+        timer.Start();
+        bool IsAuthorizeResponse = appAuthService.IsAuthorize(currentPrincipal, requiredClaims); // Need to test for all behavior of string
+        timer.Stop();
+
+
+        // Assert
+        Assert.True(timer.Elapsed.TotalSeconds <= MAX_EXECUTION_TIME_IN_SECONDS);
+        Assert.True(IsAuthorizeResponse == true);
+    }
+
+    [Fact]
+    public async void AppAuthServiceShould_ReturnThrowArgumentNullException_IfCurrentPrincpalIsNull()
+    {
+        // Arrange
+        var timer = new Stopwatch();
+
+        var appAuthService = new AppAuthService();
+  
+        AppPrincipal? currentPrincipal = null;
+
+        var requiredClaims = new Dictionary<string, string>() {
+            {"RoleName", "Admin"}
+        };
+
+        var nullExceptionIsReturn = false;
+
+
+        // Act
+        try {
+            bool IsAuthorizeResponse = appAuthService.IsAuthorize(currentPrincipal, requiredClaims); // Need to test for all behavior of string
+        } 
+        catch (ArgumentNullException)
+        {
+            nullExceptionIsReturn = true;   
+        }
+
+        // Assert
+        Assert.True(nullExceptionIsReturn);
+    }
+
+    [Fact]
+    public async void AppAuthServiceShould_ReturnThrowArgumentNullException_IfRequiredClaimsIsNull()
+    {
+        // Arrange
+        var timer = new Stopwatch();
+
+        var appAuthService = new AppAuthService();
+  
+        var currentPrincipal = new AppPrincipal();
+        currentPrincipal.UserId = "1";
+        currentPrincipal.Claims = new Dictionary<string, string>() {
+            {"RoleName", "Admin"}
+        };
+
+        IDictionary<string, string>? requiredClaims = null;
+        var nullExceptionIsReturn = false;
+
+
+        // Act
+        try {
+            bool IsAuthorizeResponse = appAuthService.IsAuthorize(currentPrincipal, requiredClaims); // Need to test for all behavior of string
+        } 
+        catch (ArgumentNullException)
+        {
+            nullExceptionIsReturn = true;   
+        }
+
+        // Assert
+        Assert.True(nullExceptionIsReturn);
+    }
+
+    [Fact]
+    public async void AppAuthServiceShould_ReturnFalse_IfUserIsNotAuthorize()
+    {
+        // Arrange
+        var timer = new Stopwatch();
+
+        var appAuthService = new AppAuthService();
+  
+        var currentPrincipal = new AppPrincipal();
+        currentPrincipal.UserId = "1";
+        currentPrincipal.Claims = new Dictionary<string, string>() {
+            {"RoleName", "Normal User"}
+        };
+
+        var requiredClaims = new Dictionary<string, string>() {
+            {"RoleName", "Admin"}
+        };
+
+
+        // Act
+        timer.Start();
+        bool IsAuthorizeResponse = appAuthService.IsAuthorize(currentPrincipal, requiredClaims); // Need to test for all behavior of string
+        timer.Stop();
+
+
+        // Assert
+        Assert.True(timer.Elapsed.TotalSeconds <= MAX_EXECUTION_TIME_IN_SECONDS);
+        Assert.True(IsAuthorizeResponse == false);
+    }
+
 
 
 }
