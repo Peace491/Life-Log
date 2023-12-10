a4d1ef4ee0254672d287429b3a56b941e99a598f
Phong Cao <phongcao0714@gmail.com>
2023-12-09 19:18:15 -0800

    Add wednesday notes

---
+++ Notes/ProjectFolder/Peace.Lifelog.Security/Contracts/IAuthenticator.cs
@@ -0,0 +1,9 @@
+﻿namespace Peace.Lifelog.Security;
+
+public interface IAuthenticator
+{
+    AppPrincipal Authenticate(AuthenticationRequest authRequest); 
+}
+
+// Identity - who you are (Authentication)
+// Principal - Who you are within a context (This is what authentication should return)
---
+++ Notes/ProjectFolder/Peace.Lifelog.Security/Models/AppPrincipal.cs
@@ -0,0 +1,11 @@
+﻿namespace Peace.Lifelog.Security;
+
+using System.Security.Claims;
+using System.Security.Principal;
+
+public class AppPrincipal : ClaimsPrincipal
+{
+    public string UserId { get; set; }
+
+    public IDictionary<string, string> Claims {get; set;}
+}
