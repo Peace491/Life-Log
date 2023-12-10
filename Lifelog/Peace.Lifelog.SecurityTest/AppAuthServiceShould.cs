e446a46cd938a9674f9bcb50b55e5cb783736058
y0shiki <yoshikiyarlagadda@gmail.com>
2023-12-10 15:04:13 -0800

    Add tests for Authentication

--- Lifelog/Peace.Lifelog.SecurityTest/AppAuthServiceShould.cs
+++ Lifelog/Peace.Lifelog.SecurityTest/AppAuthServiceShould.cs
@@ -1,7 +1,6 @@
 namespace Peace.Lifelog.SecurityTest;
-using Peace.Lifelog.Security;
 using Peace.Lifelog.DataAccess;
-
+using Peace.Lifelog.Security;
 using System.Diagnostics;
 
 public class AppAuthServiceShould
@@ -18,6 +17,7 @@ public class AppAuthServiceShould
         var createMockTableSql = $"CREATE TABLE {TABLE} ("
         + "(user_id serial  NOT NULL," +
         "proof text  NOT NULL," +
+        "claim text  NOT NULL," +
         "CONSTRAINT app_auth_service_test_pk " +
         "PRIMARY KEY (user_id)" +
         ");";
@@ -36,7 +36,127 @@ public class AppAuthServiceShould
     }
 
     // Authentication Tests
-    // TODO
+    [Fact]
+
+    public async void AppAuthServiceShould_ReturnPrincipal_ForTheUser()
+    {
+        //Arrange
+        var timer = new Stopwatch();
+
+        var appAuthService = new AppAuthService();
+
+        var createDataOnlyDAO = new CreateDataOnlyDAO();
+
+        var readDataOnlyDAO = new ReadDataOnlyDAO();
+
+        var mockUserId = "1";
+        var mockProof = "Proof";
+        var mockClaim = "Claim";
+        var mockClaimDict = new Dictionary<string, string>() {
+            {"claim", mockClaim}
+        };
+
+        var insertSql = $"INSERT INTO {TABLE} (user_id, proof, claim) VALUES ('{mockUserId}', '{mockProof}', '{mockClaim}')";
+        await createDataOnlyDAO.CreateData(insertSql);
+
+        var authRequest = new AuthenticationRequest();
+
+        authRequest.UserId = mockUserId;
+        authRequest.Proof = mockProof;
+
+
+        //Act
+        timer.Start();
+        var response = appAuthService.AuthenticateUser(authRequest);
+        timer.Stop();
+
+        //Assert
+        Assert.True(response.Claims == mockClaimDict);
+        Assert.True(timer.Elapsed.TotalSeconds <= MAX_EXECUTION_TIME_IN_SECONDS);
+
+    }
+
+    public async void AppAuthServiceShould_ReturnThrowArgumentNullException_IfUserIdIsNull()
+    {
+        //Arrange
+        var timer = new Stopwatch();
+
+        var appAuthService = new AppAuthService();
+
+        var createDataOnlyDAO = new CreateDataOnlyDAO();
+
+        var readDataOnlyDAO = new ReadDataOnlyDAO();
+
+        // mockUserId is null
+        var mockProof = "Proof";
+        var mockClaim = "Claim";
+        var mockClaimDict = new Dictionary<string, string>() {
+            {"claim", mockClaim}
+        };
+
+        var authRequest = new AuthenticationRequest();
+
+        authRequest.UserId = null;
+        authRequest.Proof = mockProof;
+
+        var nullExceptionIsReturn = false;
+
+
+        //Act
+        try
+        {
+            var response = appAuthService.AuthenticateUser(authRequest); // Need to test for all behavior of string
+        }
+        catch (ArgumentNullException)
+        {
+            nullExceptionIsReturn = true;
+        }
+
+        // Assert
+        Assert.True(nullExceptionIsReturn);
+
+    }
+
+    public async void AppAuthServiceShould_ReturnThrowArgumentNullException_IfProofIsNull()
+    {
+        //Arrange
+        var timer = new Stopwatch();
+
+        var appAuthService = new AppAuthService();
+
+        var createDataOnlyDAO = new CreateDataOnlyDAO();
+
+        var readDataOnlyDAO = new ReadDataOnlyDAO();
+
+        var mockUserId = "1";
+        // var mockProof = ""; is Null 
+        var mockClaim = "Claim";
+        var mockClaimDict = new Dictionary<string, string>() {
+            {"claim", mockClaim}
+        };
+
+        var authRequest = new AuthenticationRequest();
+
+        authRequest.UserId = mockUserId;
+        authRequest.Proof = null;
+
+        var nullExceptionIsReturn = false;
+
+
+        //Act
+        try
+        {
+            var response = appAuthService.AuthenticateUser(authRequest); // Need to test for all behavior of string
+        }
+        catch (ArgumentNullException)
+        {
+            nullExceptionIsReturn = true;
+        }
+
+        // Assert
+        Assert.True(nullExceptionIsReturn);
+
+    }
 
     // Authorization Tests
     [Fact]
@@ -46,7 +166,7 @@ public class AppAuthServiceShould
         var timer = new Stopwatch();
 
         var appAuthService = new AppAuthService();
-  
+
         var currentPrincipal = new AppPrincipal();
         currentPrincipal.UserId = "1";
         currentPrincipal.Claims = new Dictionary<string, string>() {
@@ -76,7 +196,7 @@ public class AppAuthServiceShould
         var timer = new Stopwatch();
 
         var appAuthService = new AppAuthService();
-  
+
         AppPrincipal? currentPrincipal = null;
 
         var requiredClaims = new Dictionary<string, string>() {
@@ -87,12 +207,13 @@ public class AppAuthServiceShould
 
 
         // Act
-        try {
+        try
+        {
             bool IsAuthorizeResponse = appAuthService.IsAuthorize(currentPrincipal, requiredClaims); // Need to test for all behavior of string
-        } 
+        }
         catch (ArgumentNullException)
         {
-            nullExceptionIsReturn = true;   
+            nullExceptionIsReturn = true;
         }
 
         // Assert
@@ -106,7 +227,7 @@ public class AppAuthServiceShould
         var timer = new Stopwatch();
 
         var appAuthService = new AppAuthService();
-  
+
         var currentPrincipal = new AppPrincipal();
         currentPrincipal.UserId = "1";
         currentPrincipal.Claims = new Dictionary<string, string>() {
@@ -118,12 +239,13 @@ public class AppAuthServiceShould
 
 
         // Act
-        try {
+        try
+        {
             bool IsAuthorizeResponse = appAuthService.IsAuthorize(currentPrincipal, requiredClaims); // Need to test for all behavior of string
-        } 
+        }
         catch (ArgumentNullException)
         {
-            nullExceptionIsReturn = true;   
+            nullExceptionIsReturn = true;
         }
 
         // Assert
@@ -137,7 +259,7 @@ public class AppAuthServiceShould
         var timer = new Stopwatch();
 
         var appAuthService = new AppAuthService();
-  
+
         var currentPrincipal = new AppPrincipal();
         currentPrincipal.UserId = "1";
         currentPrincipal.Claims = new Dictionary<string, string>() {
