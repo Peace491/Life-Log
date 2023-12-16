46974e76b9a48413d1d7c141997bef5f0f5f3213
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2023-12-15 16:30:45 -0800

    wont build now ;) will discuss in parkinglot

--- Lifelog/Peace.Lifelog.Security/HashService.cs
+++ Lifelog/Peace.Lifelog.Security/HashService.cs
@@ -1,16 +1,30 @@
 ﻿using DomainModels;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace Peace.Lifelog.Security;
 
 public class HashService : IHasher
 {
-    public async Task<Response> Hasher(string plaintext)
+    // Task<Response>
+    public string Hasher(string plaintext)
     {
         var response = new Response();
         try
         {
+            byte[] salt = new byte[0];
             // TODO
+            ICollection<object> hashCollection = new ICollection<string>();
+
+            hashCollection.Add(Convert.ToBase64String(KeyDerivation.Pbkdf2(plaintext,
+                                                                        salt: salt,
+                                                                        prf: KeyDerivationPrf.HMACSHA256,
+                                                                        iterationCount: 100000,
+                                                                        numBytesRequested: 256 / 8)));
+            response.Output = hashCollection;
             // hash operation will be async, could have high cost on system resources.
+            // -> after reserach, it cant be async?????
+            // return response;
             return response;
         }
         catch
---
+++ Lifelog/Peace.Lifelog.SecurityTest/HashServiceShould.cs
@@ -0,0 +1,44 @@
+﻿namespace Peace.Lifelog.SecurityTest;
+using Peace.Lifelog.DataAccess;
+using Peace.Lifelog.Security;
+using System.Diagnostics;
+
+public class HashServiceShould
+{
+
+    private const int MAX_EXECUTION_TIME_IN_SECONDS = 3001;
+    [Fact]
+    public void HasherShould_ReproduceOutputsWithSameInput()
+    {
+        // Hash function should produce the same result on the same inputs
+        // Arrange
+        string hasherInput = "jackpickleissoCOOL707";
+
+        Stopwatch timer = new Stopwatch();
+        // Act
+        timer.Start();
+        var hashOne = HashService.Hasher(hasherInput);
+        timer.Stop();
+        var hashTwo = HashService.Hasher(hasherInput);
+
+        // Assert
+        // will need to change if we are using different method sig for hasher (ideally a response object?)
+        Assert.True(hashOne == hashTwo);
+        Assert.True(timer.ElapsedMilliseconds < MAX_EXECUTION_TIME_IN_SECONDS);
+    }
+    [Fact]
+    public void HasherShouldNot_HashANullString()
+    {
+        // Arrange
+        string hasherInput = null;
+        var errorhash = "1234";
+
+        // Act/Assert
+        // This just highlights to me that we should be using a response object here
+        Assert.Fail(HashService.Hasher(hasherInput));
+
+        // Assert
+
+
+    }
+}
