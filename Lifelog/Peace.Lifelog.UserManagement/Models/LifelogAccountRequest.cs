deae8f86d7d4373a86253eebd291fcd4382edb18
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-01-29 17:07:11 -0800

    rearanged things to match vertabelo tables

--- Lifelog/Peace.Lifelog.UserManagement/Models/LifelogAccountRequest.cs
+++ Lifelog/Peace.Lifelog.UserManagement/Models/LifelogAccountRequest.cs
@@ -8,7 +8,8 @@ public class TestAccountRequest : IUserAccountRequest, IMultifactorAccountReques
     public (string Type, string Value) UserId { get; set; }
     public (string Type, string Value) UserHash { get; set; }
     public (string Type, string Value) CreationDate { get; set; }
-    public (string Type, string Value) MfaId { get; set; }
+    public (string Type, string Value) Salt { get; set; }
     public (string Type, string Value) Role { get; set; }
+    public (string Type, string Value) MfaId { get; set; }
     public (string Type, string Value) AccountStatus { get; set; } = ("AccountStatus", "Enabled");
 }
