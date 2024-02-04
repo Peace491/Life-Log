b89effe2caba4c0ced624f14be076061d26d9972
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-02-03 19:08:23 -0800

    Crude implementation of CreateLifelogUser to resolve block

--- Lifelog/Peace.Lifelog.UserManagement/Models/LifelogAccountRequest.cs
+++ Lifelog/Peace.Lifelog.UserManagement/Models/LifelogAccountRequest.cs
@@ -6,7 +6,7 @@ public class LifelogAccountRequest : IUserAccountRequest, IMultifactorAccountReq
 {
     public string ModelName { get; } = "LifelogAccount";
     public (string Type, string Value) UserId { get; set; }
-    public (string Type, string Value) UserHash { get; set; }
+    public (string Type, string Value)? UserHash { get; set; } = null;
     public (string Type, string Value) CreationDate { get; set; }
     public (string Type, string Value) Salt { get; set; }
     public (string Type, string Value) Role { get; set; }
