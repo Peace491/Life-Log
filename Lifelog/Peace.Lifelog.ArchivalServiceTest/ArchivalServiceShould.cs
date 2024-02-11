d9dfb7754035e3cbe3691986fb559a4270b7779c
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-02-11 13:03:38 -0800

    rename

---
+++ Lifelog/Peace.Lifelog.ArchivalServiceTest/ArchivalServiceShould.cs
@@ -0,0 +1,15 @@
+namespace Peace.Lifelog.ArchivalServiceTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ArchivalServiceShould_SucessfullyArchive()
+    {
+
+    }
+    [Fact]
+    public void ArchivalServiceShould_RemoveArchivedLogsFromLogTable()
+    {
+
+    }
+}
