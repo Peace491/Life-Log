d7c240f941fa49f0658e5f8b90d7270458cca80a
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-02-12 17:30:44 -0800

    outlined test cases

--- Lifelog/Peace.Lifelog.ArchivalServiceTest/ArchivalServiceShould.cs
+++ Lifelog/Peace.Lifelog.ArchivalServiceTest/ArchivalServiceShould.cs
@@ -2,13 +2,46 @@ namespace Peace.Lifelog.ArchivalServiceTest;
 
 public class ArchivalServiceShould
 {
+    // On Success
     [Fact]
-    public void ArchivalServiceShould_SucessfullyArchive()
+    public void ArchivalServiceShould_SelectLogsOlderThan30DaysOld()
     {
 
     }
     [Fact]
-    public void ArchivalServiceShould_RemoveArchivedLogsFromLogTable()
+    public void ArchivalServiceShould_DeleteArchivedLogsFromLogTable()
+    {
+
+    }
+    [Fact]
+    public void ArchivalServiceShould_UploadCompressedFiletoS3Bucket()
+    {
+
+    }
+
+    // On Failure
+    [Fact]
+    public void ArchiveShouldNot_DeleteLogsIfEncountersError()
+    {
+
+    }
+    [Fact]
+    public void ArchiveShouldNot_ContinueOnInvalidDatetime()
+    {
+
+    }
+    [Fact]
+    public void ArchiveShouldNot_ContinueOnDbInaccessable()
+    {
+
+    }
+    [Fact]
+    public void ArchiveShouldNot_ContinueOnS3InstanceInaccessable()
+    {
+
+    }
+    [Fact]
+    public void ArchiveShouldNot_ContinueOnUnableToWriteToFile()
     {
 
     }
