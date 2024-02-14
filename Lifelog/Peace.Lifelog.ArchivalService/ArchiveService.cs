9417c0e531b887eacbf64df48b2d690f53d063e3
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-02-13 21:32:25 -0800

    Revert "further outlined archive functionality, established need for helper compression function"
    This reverts commit 02f7be0603106ad24f3e28a47a40e7728b48e14a.

--- Lifelog/Peace.Lifelog.ArchivalService/ArchiveService.cs
+++ Lifelog/Peace.Lifelog.ArchivalService/ArchiveService.cs
@@ -1,8 +1,4 @@
 ﻿using DomainModels;
-using System.IO;
-using System.IO.Compression;
-using Peace.Lifelog.DataAccess;
-using Peace.Lifelog.Logging;
 
 namespace Peace.Lifelog.ArchivalService;
 
@@ -10,44 +6,23 @@ public class ArchivalService : IArchive
 {
     public Response Archive(DateTime dateTime)
     {
-        var createOnlyDAO = new CreateDataOnlyDAO();
-        var logTarget = new LogTarget(createOnlyDAO);
-        var logger = new Logging.Logging(logTarget);
-
-        var readOnlyDAO = new ReadDataOnlyDAO();
-        var deleteDataDAO = new DeleteDataOnlyDAO(); 
-
         var response = new Response();
 
         response.HasError = true;
         response.Output = ["not implemented"];
 
         // Read Logs older than 30 days old
-        var readArchivableLogs = "SELECT * FROM LifelogDB.Logs WHERE LogTimestamp < DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY);";
 
-        // Put all info from each log into a string
-        var archivalString = "";
-        
+        // Write result to .txt file
 
-        // Write string result to .txt file
-        var filePath = "../"; 
-        var finalPath = "../";
+        // Compress resulting .txt file
 
-        File.WriteAllText(filePath, archivalString);
-
-        // Compress resulting .txt file & save to final location off db (gold plating: s3 bucket)
-        CompressFile(filePath, finalPath);
+        // Write to s3 bucket
 
         // Offload/Delete logs older than 30 days old
-        var offloadArchivedLogs = "DELETE FROM LifelogDB.Logs WHERE LogTimestamp < DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY);";
 
         // Log archival operation 
-        // logger.CreateLog(TABLE, hash, level, category, message);
-        
-        return response;
-    }
-    static void CompressFile(string sourcePath, string destinationPath)
-    {
 
+        return response;
     }
 }
