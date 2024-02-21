bbe083692869a8d79cd8247ce3c9513b7ebeb53c
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-02-21 13:02:30 -0800

    Note - Delete logs is currently not in logic

--- src/backend/Lifelog/Peace.Lifelog.ArchivalService/ArchiveService.cs
+++ src/backend/Lifelog/Peace.Lifelog.ArchivalService/ArchiveService.cs
@@ -15,87 +15,102 @@ public class ArchivalService : IArchive
         response.HasError = true;
 
         // Init nessesary DAOS
+        var createDataOnlyDAO = new CreateDataOnlyDAO();
         var readDAO = new ReadDataOnlyDAO();
         var deleteDAO = new DeleteDataOnlyDAO();
 
         // Init Logger keep having logger init issues
         var createOnlyDAO = new CreateDataOnlyDAO();
         var logTarget = new LogTarget(createOnlyDAO);
-        // var logger = new Logging(logTarget);
+        var logger = new Logging.Logging(logTarget);
 
         // Init SQL 
         string SELECT = "SELECT "; 
         string DELETE = "DELETE "; 
         string validLogs = "* FROM Logs WHERE LogTimestamp > DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY);";
 
-        // Get the target folder
-        DirectoryInfo currentDirectory = new DirectoryInfo(Environment.CurrentDirectory);
-        for (int i = 0; i < 7; i++)
-        {
-            currentDirectory = currentDirectory.Parent;
-        }
-        DirectoryInfo targetDirectory = new DirectoryInfo(Path.Combine(currentDirectory.FullName, "Project Documents"));
-        DirectoryInfo levelDownLocation = new DirectoryInfo(Path.Combine(targetDirectory.FullName, "Archive"));
-
-        DirectoryInfo txtLocation = new DirectoryInfo(Path.Combine(levelDownLocation.FullName, "Archive txt"));
-        DirectoryInfo zipLocation = new DirectoryInfo(Path.Combine(levelDownLocation.FullName, "Archive zip"));
-
-
-
-        // Get date for archive txt and zip name
-        DateTime today = DateTime.Today;
-        string year = today.Year.ToString();
-        string month = today.Month.ToString("00");
+        // Init archive filenames from input
+        string year = dateTime.Year.ToString();
+        string month = dateTime.Month.ToString("00");
 
         string txtName = $"{year}_{month}archive.txt";
         string archiveName = $"{year}_{month}archive.zip"; 
-
-        // Read Logs older than 30 days old
-        var readResponse = await readDAO.ReadData(SELECT + validLogs, 10000);
-       // var path = Path.Combine(txtLocation.FullName, txtName);
-
-        // Write result to .txt file
-        // using (StreamWriter sw = File.CreateText(path))
-        // /Users/jackpickle/dev/repos/Life-Log/Project Documents/Archive
-        // /Users/jackpickle/dev/repos/Life-Log/src/backend/Project Documents/Archive/2024_02archive.txt
-
-        using (StreamWriter outputFile = new StreamWriter(Path.Combine(txtLocation.FullName, txtName)))
-        {
-            foreach (List<Object> readLogData in readResponse.Output)
-            {
-                string archiveFLine = ""; 
-                foreach (object element in readLogData)
-                {
-                    archiveFLine += element.ToString() + " ";
-                } 
-                outputFile.WriteLine(archiveFLine);
-            }
-            outputFile.Close();
-        }
         
-
-        // Compress resulting .txt file
-
-        // Read txt
-        using (FileStream sourceFileStream = File.OpenRead(Path.Combine(txtLocation.FullName, txtName)))
+        // 
+        string level = "ERROR";
+        string category = "View";
+        try // Protect against failure
         {
-            // access archive loc
-            using (FileStream compressedFileStream = File.Create(Path.Combine(zipLocation.FullName, archiveName)))
+            // Get the target folder
+            DirectoryInfo currentDirectory = new DirectoryInfo(Environment.CurrentDirectory);
+        
+            for (int i = 0; i < 7; i++)
             {
-                // uze gzipstream to compress file to archive location stream
-                using (GZipStream gzipStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                currentDirectory = currentDirectory.Parent;
+            }
+
+            DirectoryInfo targetDirectory = new DirectoryInfo(Path.Combine(currentDirectory.FullName, "Project Documents"));
+            DirectoryInfo levelDownLocation = new DirectoryInfo(Path.Combine(targetDirectory.FullName, "Archive"));
+
+            DirectoryInfo txtLocation = new DirectoryInfo(Path.Combine(levelDownLocation.FullName, "Archive txt"));
+            DirectoryInfo zipLocation = new DirectoryInfo(Path.Combine(levelDownLocation.FullName, "Archive zip"));
+
+            // Read Logs older than 30 days old
+            category = "Persistent Data Store";
+            var readResponse = await readDAO.ReadData(SELECT + validLogs, 10000);
+
+            category = "Server";
+            // Write result of query to .txt file
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(txtLocation.FullName, txtName)))
+            {
+                foreach (List<Object> readLogData in readResponse.Output)
                 {
-                    sourceFileStream.CopyTo(gzipStream);
+                    string archiveFLine = ""; 
+                    foreach (object element in readLogData)
+                    {
+                        archiveFLine += element.ToString() + " ";
+                    } 
+                    archiveFLine += ";";
+                    outputFile.WriteLine(archiveFLine);
+                }
+                outputFile.Close();
+            }
+
+
+            // Compress resulting .txt file
+            using (FileStream sourceFileStream = File.OpenRead(Path.Combine(txtLocation.FullName, txtName)))
+            {
+                // access archive loc
+                using (FileStream compressedFileStream = File.Create(Path.Combine(zipLocation.FullName, archiveName)))
+                {
+                    // uze gzipstream to compress file to archive location stream
+                    using (GZipStream gzipStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                    {
+                        sourceFileStream.CopyTo(gzipStream);
+                    }
                 }
             }
-        }     
 
-        // Offload/Delete logs older than 30 days old
-        // var deleteResponse = await readDAO.ReadData(DELETE + validLogs);
+            // Offload/Delete logs older than 30 days old
+            category = "Persistent Data Store";
+            // var deleteResponse = await readDAO.ReadData(DELETE + validLogs);
 
-        // TODO: Log archival operation 
+            // Log archival operation 
+            level = "Info";
+            category = "View";
 
-        response.HasError = false; 
+            response.HasError = false;
+            _ = await logger.CreateLog("Logs", "TxT3KzlpTG0ExziT6GhXfJDStrAssjrEZjbe14UBfvU=", level, category, response.ErrorMessage);
+        } 
+        catch (Exception exception)
+        {
+            response.HasError = true;
+            response.ErrorMessage = exception.GetBaseException().Message; 
+
+            // Log on failure
+            _ = await logger.CreateLog("Logs", "TxT3KzlpTG0ExziT6GhXfJDStrAssjrEZjbe14UBfvU=", level, category, response.ErrorMessage);
+        }
+        
         return response;
     }
 }
