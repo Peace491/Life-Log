d96fdff45b21fa5fd444dd9799508b12c213a7d5
Phong Cao <phongcao0714@gmail.com>
2024-03-05 15:08:15 -0800

    Implement LLI front page

---
+++ src/frontend/LLIManagementPage/lli.js
@@ -0,0 +1,139 @@
+'use strict';
+
+// Immediately Invoke Function Execution (IIFE or IFE)
+// Protects functions from being exposed to the global object
+(function (root, ajaxClient) {
+    // Dependency check
+    const isValid = root && ajaxClient;
+
+    console.log(root)
+    console.log(ajaxClient)
+
+    if(!isValid){
+        // Handle missing dependencies
+        alert("Missing dependencies");
+    }
+
+
+    // NOT exposed to the global object ("Private" functions)
+    function getAllLLI() {
+        const webServiceUrl = 'http://localhost:8080/lli';
+        let getUrl = webServiceUrl + '/getAllLLI?userHash=System';
+        let request = ajaxClient.get(getUrl);
+        
+        return new Promise((resolve, reject) => {
+            request.then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                let output = data.output;
+                resolve(output);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    }
+
+    // NOT exposed to the global object ("Private" functions)
+    function sendDataHandler(url) {
+        var request = ajaxClient.send(url, {
+                data: ['Alice', 'Bob', 'John'] // Hard-coding data
+            });
+
+        const contentElement = document.getElementsByClassName('dynamic-content')[0];
+
+        request
+            .then(function (response) {
+                
+                const timestamp = new Date();
+
+                contentElement.innerHTML = response.data + " " + timestamp.toString(); 
+            })
+            .catch(function (error) {
+                console.log("Send", url, error.response.data); // Payload error message
+
+                contentElement.innerHTML = error; // Only showing top level error message
+            });
+    }
+
+
+    root.myApp = root.myApp || {};
+
+    // Show or Hide private functions
+    //root.myApp.getData = getDataHandler;
+    //root.myApp.sendData = sendDataHandler;
+
+    // Initialize the current view by attaching event handlers 
+    function init() {
+        let lliContentContainer = document.getElementsByClassName("current-lli-content-container")[0]
+
+        getAllLLI().then(function (completedLLIList) {
+            
+
+            completedLLIList.forEach(lli => {
+                
+                let lliHTML = createLLIHTML(lli.title, lli.deadline.substring(0, lli.deadline.indexOf(" ")), lli.category, lli.description)
+                
+                lliContentContainer.append(lliHTML)
+            });
+            
+        });
+    }
+
+    init();
+
+})(window, window.ajaxClient);
+
+function createLLIHTML(title, deadline, category, description) {
+    // Create elements
+    var lliContainer = document.createElement("div");
+    lliContainer.classList.add("lli");
+
+    var titleContainer = document.createElement("div");
+    titleContainer.classList.add("lli-title-container");
+
+    var titleElement = document.createElement("h2");
+    titleElement.textContent = title;
+
+    var mainContentContainer = document.createElement("div");
+    mainContentContainer.classList.add("lli-main-content-container");
+
+    var deadlineContainer = document.createElement("div");
+    deadlineContainer.classList.add("lli-deadline-container");
+
+    var deadlineElement = document.createElement("h2");
+    deadlineElement.textContent = "Deadline: " + deadline;
+
+    var categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("lli-category-container");
+
+    var categoryElement = document.createElement("h2");
+    categoryElement.textContent = category;
+
+    var descriptionContainer = document.createElement("div");
+    descriptionContainer.classList.add("lli-description-container");
+
+    var descriptionTitle = document.createElement("h2");
+    descriptionTitle.textContent = "Description";
+
+    var descriptionParagraph = document.createElement("p");
+    descriptionParagraph.textContent = description;
+
+    // Append elements
+    descriptionContainer.appendChild(descriptionTitle);
+    descriptionContainer.appendChild(descriptionParagraph);
+
+    deadlineContainer.appendChild(deadlineElement);
+    categoryContainer.appendChild(categoryElement);
+
+    mainContentContainer.appendChild(deadlineContainer);
+    mainContentContainer.appendChild(categoryContainer);
+
+    titleContainer.appendChild(titleElement);
+
+    lliContainer.appendChild(titleContainer);
+    lliContainer.appendChild(mainContentContainer);
+    lliContainer.appendChild(descriptionContainer);
+
+    // Return the created element
+    return lliContainer;
+}
---
+++ src/frontend/Resources/Ajax/fetch-ajax.js
@@ -0,0 +1,37 @@
+function get(url) {
+
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer-when-downgrade',
+    };
+
+    return fetch(url, options);
+}
+
+function send(url, data) {
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'default',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer-when-downgrade',
+        body: ''
+    };
+
+    return fetch(url, options);
+}
+
+window.ajaxClient = {
+    get: get
+}
