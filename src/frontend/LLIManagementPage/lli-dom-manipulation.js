b941f8cb025cfc13e810b897f3bd31d49cf71596
Phong Cao <phongcao0714@gmail.com>
2024-03-08 19:44:52 -0800

    Refine lli front end design details

---
+++ src/frontend/LLIManagementPage/lli-dom-manipulation.js
@@ -0,0 +1,318 @@
+function createLLIHTML(lli) {
+    console.log(lli)
+
+    // Create div element with class "lli" and "expanded-lli"
+    const lliDiv = document.createElement('div');
+    lliDiv.classList.add('lli');
+    lliDiv.id = lli.lliid;
+
+    // Create non-hidden content div
+    const nonHiddenContentDiv = document.createElement('div');
+    nonHiddenContentDiv.classList.add('lli-non-hidden-content');
+
+    // Title container
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('lli-title-container');
+    const titleHeading = document.createElement('h2');
+    titleHeading.id = 'title' + lli.lliid;
+    titleHeading.textContent = lli.title || 'Enter Title';
+    titleContainer.appendChild(titleHeading);
+
+    // Main content container
+    const mainContentContainer = document.createElement('div');
+    mainContentContainer.classList.add('lli-main-content-container');
+
+    // Deadline container
+    const deadlineContainer = document.createElement('div');
+    deadlineContainer.classList.add('lli-deadline-container');
+    const deadlineHeading = document.createElement('h2');
+    deadlineHeading.innerHTML = `<span style="font-weight: 600;">Deadline:</span> ${lli.deadline.substring(0, lli.deadline.indexOf(" "))}`;
+    deadlineContainer.appendChild(deadlineHeading);
+
+    // Category container
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('lli-category-container');
+    const categoryHeading = document.createElement('h2');
+    categoryHeading.id = 'category' + lli.lliid;
+    categoryHeading.innerHTML = `<span style="font-weight: 600;"> ${lli.category || 'Mental Health'}`
+    categoryContainer.appendChild(categoryHeading);
+
+    // Append deadline and category containers to main content container
+    mainContentContainer.appendChild(deadlineContainer);
+    mainContentContainer.appendChild(categoryContainer);
+
+    // Description container
+    const descriptionContainer = document.createElement('div');
+    descriptionContainer.classList.add('lli-description-container');
+    const descriptionHeading = document.createElement('h2');
+    descriptionHeading.textContent = 'Description';
+    const descriptionParagraph = document.createElement('p');
+    descriptionParagraph.textContent = lli.description || 'A paragraph';
+    descriptionParagraph.id = 'description' + lli.lliid
+    descriptionContainer.appendChild(descriptionHeading);
+    descriptionContainer.appendChild(descriptionParagraph);
+
+    // Append title, main content, and description containers to non-hidden content div
+    nonHiddenContentDiv.appendChild(titleContainer);
+    nonHiddenContentDiv.appendChild(mainContentContainer);
+    nonHiddenContentDiv.appendChild(descriptionContainer);
+
+    // Create hidden content div
+    const hiddenContentDiv = document.createElement('div');
+    hiddenContentDiv.classList.add('lli-hidden-content', 'hidden');
+
+    // Button container
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('lli-button-container');
+
+    // Delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.id = 'delete-lli-button';
+
+    buttonContainer.appendChild(deleteButton);
+
+    // Edit button
+    const editButton = document.createElement('button');
+    editButton.id = 'edit-lli-button';
+
+    buttonContainer.appendChild(editButton);
+
+    // Close button
+    const closeButton = document.createElement('button');
+    closeButton.id = 'close-lli-button';
+
+    buttonContainer.appendChild(closeButton);
+
+    // Append button container to hidden content div
+    hiddenContentDiv.appendChild(buttonContainer);
+
+    // Hidden fields container
+    const hiddenFieldsContainer = document.createElement('div');
+    hiddenFieldsContainer.classList.add('lli-hidden-fields-container');
+
+    // Hidden required fields container
+    const hiddenRequiredFieldsContainer = document.createElement('div');
+    hiddenRequiredFieldsContainer.classList.add('lli-hidden-required-fields-container')
+
+    // Status container
+    const statusContainer = document.createElement('div');
+    statusContainer.classList.add('lli-status-container');
+    const statusHeading = document.createElement('h2');
+    statusHeading.innerHTML = `<span style="font-weight: 600;">Status: </span>`;
+    const statusValue = document.createElement('h2');
+    statusValue.id = 'status' + lli.lliid;
+    statusValue.textContent = lli.status || 'Active';
+    statusContainer.appendChild(statusHeading);
+    statusContainer.appendChild(statusValue);
+
+    // Visibility container
+    const visibilityContainer = document.createElement('div');
+    visibilityContainer.classList.add('lli-visibility-container');
+    const visibilityHeading = document.createElement('h2');
+    visibilityHeading.innerHTML = `<span style="font-weight: 600;">Visibility: </span>`;
+    const visibilityValue = document.createElement('h2');
+    visibilityValue.id = 'visibility' + lli.lliid;
+    visibilityValue.textContent = lli.visibility || 'Public';
+    visibilityContainer.appendChild(visibilityHeading);
+    visibilityContainer.appendChild(visibilityValue);
+
+    // Append status and visibility containers to hidden fields container
+    hiddenRequiredFieldsContainer.appendChild(statusContainer);
+    hiddenRequiredFieldsContainer.appendChild(visibilityContainer);
+    hiddenFieldsContainer.appendChild(hiddenRequiredFieldsContainer)
+
+    const hiddenNonRequiredFieldsContainer = document.createElement('div')
+    hiddenNonRequiredFieldsContainer.classList.add('lli-hidden-non-required-fields-container')
+
+    // Cost container
+    const costHeading = document.createElement('h2');
+    costHeading.innerHTML = `<span style="font-weight: 600;">Cost:</span> $<span id="cost-input${lli.lliid}">${lli.cost || '100'}</span>`;
+
+    // Recurrence container
+    const recurrenceContainer = document.createElement('div')
+    recurrenceContainer.classList.add('lli-recurrence-container')
+
+    const recurrenceHeading = document.createElement('h2');
+    recurrenceHeading.innerHTML = '<span style="font-weight: 600;">Recurring:</span>';
+    const recurrenceValue = document.createElement('h2')
+    recurrenceValue.innerHTML = `<span id="recurrence-option">${lli.recurrence.frequency || 'None'}</span>`
+    recurrenceValue.id = 'lli-recurrence-value' + lli.lliid
+
+    recurrenceContainer.appendChild(recurrenceHeading)
+    recurrenceContainer.appendChild(recurrenceValue)
+
+    // Append cost and recurrence containers to hidden fields container
+
+    hiddenNonRequiredFieldsContainer.appendChild(costHeading);
+    hiddenNonRequiredFieldsContainer.appendChild(recurrenceContainer);
+    hiddenFieldsContainer.appendChild(hiddenNonRequiredFieldsContainer)
+
+    // Media container
+    const mediaContainer = document.createElement('div');
+    mediaContainer.classList.add('lli-media-container');
+    const mediaImg = document.createElement('img');
+    mediaImg.src = lli.media || './Assets/default-pic.svg';
+    mediaImg.alt = '';
+    mediaContainer.appendChild(mediaImg);
+
+    // Append button, hidden fields, and media containers to hidden content div
+    hiddenContentDiv.appendChild(buttonContainer);
+    hiddenContentDiv.appendChild(hiddenFieldsContainer);
+    hiddenContentDiv.appendChild(mediaContainer);
+
+    // Append non-hidden content and hidden content divs to the main lli div
+    lliDiv.appendChild(nonHiddenContentDiv);
+    lliDiv.appendChild(hiddenContentDiv);
+
+    // Add event listeners
+    // Expand the lli
+    lliDiv.addEventListener('click', expandDiv)
+
+    // Close the lli
+    closeButton.addEventListener('click', async function () {
+        encloseDiv()
+
+        await new Promise(r => setTimeout(r, 100)) // Sleep for 1 ms so the function doesnt get call right away
+        lliDiv.addEventListener('click', expandDiv)
+    })
+
+    function expandDiv() {
+        lliDiv.className = 'lli expanded-lli'
+        hiddenContentDiv.className = 'lli-hidden-content'
+        lliDiv.removeEventListener('click', expandDiv)
+    }
+
+    function encloseDiv() {
+        lliDiv.className = 'lli'
+        hiddenContentDiv.className = 'lli-hidden-content hidden'
+    }
+
+    editButton.addEventListener('click', function () {
+        convertLLIToEditMode(lli.lliid)
+    })
+
+    deleteButton.addEventListener('click', function () {
+        let response = deleteLLI(lli.lliid).then(function (output) {
+            if (output.hasError == false) {
+                lliDiv.parentNode.removeChild(lliDiv)
+            }
+
+        })
+        // Need to check if the previous call succeed before calling this
+
+    })
+
+    return lliDiv;
+}
+
+function convertLLIToEditMode(id) {
+    const lliDiv = document.getElementById(id);
+    if (!lliDiv) {
+        console.error('LLI div with specified id not found.');
+        return;
+    }
+
+    // Change title to contenteditable
+    const title = lliDiv.querySelector('#title');
+    if (title) {
+        title.contentEditable = true;
+    }
+
+    // Change deadline to input type date
+    const deadline = lliDiv.querySelector('.lli-deadline-container h2');
+    if (deadline) {
+        const dateInput = document.createElement('input');
+        dateInput.classList.add('date-input')
+        dateInput.id = 'date-input' + id
+        dateInput.type = 'date';
+        deadline.innerHTML = '<span style="font-weight: 600;">Deadline:</span> ';
+        deadline.appendChild(dateInput);
+    }
+
+    // Change category to select options
+    const category = lliDiv.querySelector('.lli-category-container');
+    if (category) {
+        const categorySelect = document.createElement('select');
+        categorySelect.id = 'category-select' + id;
+        const categories = ['Mental Health', 'Physical Health', 'Outdoor', 'Sport', 'Art', 'Hobby', 'Thrill', 'Travel', 'Volunteering', 'Food'];
+        categories.forEach(cat => {
+            const option = document.createElement('option');
+            option.value = cat;
+            option.textContent = cat;
+            categorySelect.appendChild(option);
+        });
+        category.innerHTML = '';
+        category.appendChild(categorySelect);
+    }
+
+    // Change description to contenteditable
+    const description = lliDiv.querySelector('.lli-description-container p');
+    if (description) {
+        description.contentEditable = true;
+    }
+
+    // Change status to select options
+    const status = lliDiv.querySelector('.lli-status-container');
+    if (status) {
+        const statusSelect = document.createElement('select');
+        statusSelect.classList.add('status-select')
+        statusSelect.id = 'status-select' + id;
+        const statusOptions = ['Active', 'Completed', 'Postponed'];
+        statusOptions.forEach(opt => {
+            const option = document.createElement('option');
+            option.value = opt;
+            option.textContent = opt;
+            statusSelect.appendChild(option);
+        });
+        status.innerHTML = '<h2><span style="font-weight: 600;">Status: </span></h2>';
+        status.appendChild(statusSelect);
+    }
+
+    // Change visibility to select options
+    const visibility = lliDiv.querySelector('.lli-visibility-container');
+    if (visibility) {
+        const visibilitySelect = document.createElement('select');
+        visibilitySelect.classList.add('visibility-select')
+        visibilitySelect.id = 'visibility-select' + id;
+        const visibilityOptions = ['Public', 'Private'];
+        visibilityOptions.forEach(opt => {
+            const option = document.createElement('option');
+            option.value = opt;
+            option.textContent = opt;
+            visibilitySelect.appendChild(option);
+        });
+        visibility.innerHTML = '<h2><span style="font-weight: 600;">Visibility: </span></h2>';
+        visibility.appendChild(visibilitySelect);
+    }
+
+    // Change cost to contenteditable
+    const cost = lliDiv.querySelector('#cost-input');
+    if (cost) {
+        cost.contentEditable = true;
+    }
+
+    // Change recurrence to select options
+    const recurrence = lliDiv.querySelector('#lli-recurrence-value' + id);
+    if (recurrence) {
+        const recurrenceSelect = document.createElement('select');
+        recurrenceSelect.classList.add('recurrence-select')
+        recurrenceSelect.id = 'recurrence-select' + id;
+        const recurrenceOptions = ['Off', 'Weekly', 'Monthly', 'Yearly'];
+        recurrenceOptions.forEach(opt => {
+            const option = document.createElement('option');
+            option.value = opt;
+            option.textContent = opt;
+            recurrenceSelect.appendChild(option);
+        });
+        recurrence.parentNode.appendChild(recurrenceSelect);
+        recurrence.parentNode.removeChild(recurrence)
+        
+    }
+
+    return lliDiv.outerHTML;
+}
+
+export {
+    createLLIHTML,
+    convertLLIToEditMode
+}
--- src/frontend/LLIManagementPage/lli.js
+++ src/frontend/LLIManagementPage/lli.js
@@ -1,3 +1,5 @@
+import { createLLIHTML, convertLLIToEditMode } from './lli-dom-manipulation.js'
+
 'use strict';
 
 // Immediately Invoke Function Execution (IIFE or IFE)
@@ -6,7 +8,7 @@
     // Dependency check
     const isValid = root && ajaxClient;
 
-    if(!isValid){
+    if (!isValid) {
         // Handle missing dependencies
         alert("Missing dependencies");
     }
@@ -15,10 +17,10 @@
 
     // NOT exposed to the global object ("Private" functions)
     function getAllLLI() {
-        
+
         let getUrl = webServiceUrl + '/getAllLLI?userHash=System';
         let request = ajaxClient.get(getUrl);
-        
+
         return new Promise((resolve, reject) => {
             request.then(function (response) {
                 return response.json();
@@ -34,17 +36,17 @@
     // NOT exposed to the global object ("Private" functions)
     function sendDataHandler(url) {
         var request = ajaxClient.send(url, {
-                data: ['Alice', 'Bob', 'John'] // Hard-coding data
-            });
+            data: ['Alice', 'Bob', 'John'] // Hard-coding data
+        });
 
         const contentElement = document.getElementsByClassName('dynamic-content')[0];
 
         request
             .then(function (response) {
-                
+
                 const timestamp = new Date();
 
-                contentElement.innerHTML = response.data + " " + timestamp.toString(); 
+                contentElement.innerHTML = response.data + " " + timestamp.toString();
             })
             .catch(function (error) {
                 console.log("Send", url, error.response.data); // Payload error message
@@ -52,12 +54,12 @@
                 contentElement.innerHTML = error; // Only showing top level error message
             });
     }
-    
+
     function deleteLLI(lliid) {
         let deleteUrl = webServiceUrl + '/deleteLLI?userHash=System&lliID=' + lliid;
         console.log(deleteUrl)
         let request = ajaxClient.del(deleteUrl);
-        
+
         return new Promise((resolve, reject) => {
             request.then(function (response) {
                 return response.json();
@@ -69,153 +71,11 @@
         })
     }
 
-    function createLLIHTML(lli) {
-        const container = document.createElement('div');
-        container.className = 'lli';
-    
-        const nonHiddenContent = document.createElement('div');
-        nonHiddenContent.className = 'lli-non-hidden-content';
-    
-        const titleContainer = document.createElement('div');
-        titleContainer.className = 'lli-title-container';
-        const title = document.createElement('h2');
-        title.textContent = lli.title;
-        titleContainer.appendChild(title);
-        nonHiddenContent.appendChild(titleContainer);
-    
-        const mainContentContainer = document.createElement('div');
-        mainContentContainer.className = 'lli-main-content-container';
-    
-        const deadlineContainer = document.createElement('div');
-        deadlineContainer.className = 'lli-deadline-container';
-        const deadline = document.createElement('h2');
-        deadline.innerHTML = '<span style="font-weight: 600;">Deadline:</span> ' + lli.deadline.substring(0, lli.deadline.indexOf(" "));
-        deadlineContainer.appendChild(deadline);
-        mainContentContainer.appendChild(deadlineContainer);
-    
-        const categoryContainer = document.createElement('div');
-        categoryContainer.className = 'lli-category-container';
-        const category = document.createElement('h2');
-        category.innerHTML = '<span style="font-weight: 600;">' + lli.category + '</span>';
-        categoryContainer.appendChild(category);
-        mainContentContainer.appendChild(categoryContainer);
-    
-        nonHiddenContent.appendChild(mainContentContainer);
-    
-        const descriptionContainer = document.createElement('div');
-        descriptionContainer.className = 'lli-description-container';
-        const descriptionTitle = document.createElement('h2');
-        descriptionTitle.textContent = 'Description';
-        const description = document.createElement('p');
-        description.textContent = lli.description;
-        descriptionContainer.appendChild(descriptionTitle);
-        descriptionContainer.appendChild(description);
-        nonHiddenContent.appendChild(descriptionContainer);
-    
-        container.appendChild(nonHiddenContent);
-    
-        const hiddenContent = document.createElement('div');
-        hiddenContent.className = 'lli-hidden-content hidden';
-    
-        const buttonContainer = document.createElement('div');
-        buttonContainer.className = 'lli-button-container';
-        const deleteButton = document.createElement('button');
-        deleteButton.id = 'delete-lli-button';
-        const editButton = document.createElement('button');
-        editButton.id = 'edit-lli-button';
-        const closeButton = document.createElement('button');
-        closeButton.id = 'close-lli-button';
-        buttonContainer.appendChild(deleteButton);
-        buttonContainer.appendChild(editButton);
-        buttonContainer.appendChild(closeButton);
-        hiddenContent.appendChild(buttonContainer);
-    
-        const hiddenFieldsContainer = document.createElement('div');
-        hiddenFieldsContainer.className = 'lli-hidden-fields-container';
-    
-        const hiddenRequiredFieldsContainer = document.createElement('div');
-        hiddenRequiredFieldsContainer.className = 'lli-hidden-required-fields-container';
-        const status = document.createElement('h2');
-        status.innerHTML = '<span style="font-weight: 600;">Status:</span> ' + lli.status;
-        const visibility = document.createElement('h2');
-        visibility.innerHTML = '<span style="font-weight: 600;">Visibility:</span> ' + lli.visibility;
-        hiddenRequiredFieldsContainer.appendChild(status);
-        hiddenRequiredFieldsContainer.appendChild(visibility);
-        hiddenFieldsContainer.appendChild(hiddenRequiredFieldsContainer);
-    
-        const hiddenNonRequiredFieldsContainer = document.createElement('div');
-        hiddenNonRequiredFieldsContainer.className = 'lli-hidden-non-required-fields-container';
-        const cost = document.createElement('h2');
-        cost.innerHTML = '<span style="font-weight: 600;">Cost:</span> $' + lli.cost;
-        const recurrencyFrequency = document.createElement('h2');
-        recurrencyFrequency.innerHTML = '<span style="font-weight: 600;">Recurrency Frequency:</span> ' + lli.recurrencyFrequency;
-        hiddenNonRequiredFieldsContainer.appendChild(cost);
-        hiddenNonRequiredFieldsContainer.appendChild(recurrencyFrequency);
-        hiddenFieldsContainer.appendChild(hiddenNonRequiredFieldsContainer);
-    
-        hiddenContent.appendChild(hiddenFieldsContainer);
-    
-        const mediaContainer = document.createElement('div');
-        mediaContainer.className = 'lli-media-container';
-        const image = document.createElement('img');
-        image.src = './Assets/default-pic.svg';
-        image.alt = '';
-        mediaContainer.appendChild(image);
-        hiddenContent.appendChild(mediaContainer);
-    
-        container.appendChild(hiddenContent);
-    
-        // Add event listeners
-        // Expand the lli
-        container.addEventListener('click', expandDiv)
-    
-        // Close the lli
-        closeButton.addEventListener('click', async function() {
-            encloseDiv()
-            
-            await new Promise(r => setTimeout(r, 100)) // Sleep for 1 ms so the function doesnt get call right away
-            container.addEventListener('click', expandDiv)
-        })
-    
-        function expandDiv() {
-            container.className = 'lli expanded-lli'
-            hiddenContent.className = 'lli-hidden-content'
-            container.removeEventListener('click',expandDiv)
-        }
-    
-        function encloseDiv() {
-            container.className = 'lli'
-            hiddenContent.className = 'lli-hidden-content hidden'
-        }
-
-        deleteButton.addEventListener('click', function() {
-            let response = deleteLLI(lli.lliid).then(function(output) {
-                if (output.hasError == false) {
-                    container.parentNode.removeChild(container)
-                }
-                
-            })
-            // Need to check if the previous call succeed before calling this
-            
-            
-            
-        })
-        
-        return container;
-    }
-
-
-    root.myApp = root.myApp || {};
-
-    // Show or Hide private functions
-    //root.myApp.getData = getDataHandler;
-    //root.myApp.sendData = sendDataHandler;
-
-    // Initialize the current view by attaching event handlers 
-    function init() {
+    function showLLI() {
         let lliContentContainer = document.getElementsByClassName("current-lli-content-container")[0]
         let finishedLLIContentContainer = document.getElementsByClassName("finished-lli-content-container")[0]
 
+        // Get initial lli
         getAllLLI().then(function (completedLLIList) {
             completedLLIList.reverse().forEach(lli => {
                 let lliHTML = createLLIHTML(lli);
@@ -229,12 +89,29 @@
         });
     }
 
+    function setupAddButton() {
+        let addButton = document.getElementById('add-lli-button')
+        addButton.addEventListener('click', function() {
+            var addLLITemplate = document.getElementById('create-lli-template')
+            addLLITemplate.classList.remove('hidden')
+        })
+    }
+
+    root.myApp = root.myApp || {};
+
+    // Initialize the current view by setting up data and attaching event handlers 
+    function init() {
+        setupAddButton();
+        showLLI();
+
+    }
+
     init();
 
 })(window, window.ajaxClient);
 
 function expandLLIHTML(container, hiddenContent) {
-    
+
 
 }
 
