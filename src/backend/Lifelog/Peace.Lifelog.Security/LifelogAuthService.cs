f809e2b1d96426d95acbfcee1c54055e6d4efdf6
Phong Cao <phongcao0714@gmail.com>
2024-03-10 23:12:13 -0700

    Set up Lifelog Authentication

---
+++ src/backend/Lifelog/Peace.Lifelog.Security/LifelogAuthService.cs
@@ -0,0 +1,25 @@
+﻿namespace Peace.Lifelog.Security;
+
+public class LifelogAuthService
+{
+    public Task<AppPrincipal>? AuthenticateLifelogUser(string UserHash, string OTPHash)
+    {
+        if (UserHash == string.Empty) {
+            return null;
+        }
+        if (OTPHash == string.Empty) {
+            return null;
+        }
+
+        var lifelogAuthenticationRequest = new LifelogAuthenticationRequest();
+        lifelogAuthenticationRequest.UserId = ("UserHash", UserHash);
+        lifelogAuthenticationRequest.Proof = ("OTPHash", OTPHash);
+        lifelogAuthenticationRequest.Claims = ("Role", "");
+
+        var appAuthService = new AppAuthService();
+        var principal = appAuthService.AuthenticateUser(lifelogAuthenticationRequest);
+
+        return principal;
+    }
+
+}
