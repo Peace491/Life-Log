4762bb8d2c703a0c31e03a5bacc64c9970c21f31
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-03-18 13:15:34 -0700

    handle warnings

--- src/backend/Lifelog/Peace.Lifelog.RE/REService.cs
+++ src/backend/Lifelog/Peace.Lifelog.RE/REService.cs
@@ -30,26 +30,33 @@ namespace Peace.Lifelog.RE
             Dictionary<string, int> userScoresLLI = new Dictionary<string, int>();
             try
             {
+                // Get user's user form info
                 var formResponse = await readDataOnlyDAO.ReadData(sqlGetUserFormData);
                 userScores = scoreInit(formResponse);
 
+                // Get user's LLI info
                 var userLLIResponse = await readDataOnlyDAO.ReadData(sqlGetUserLLI);
                 userScoresLLI = scoreLLI(userScores, userLLIResponse);
+                
+                // Get users top two categories from LLI info
+                var usersTopTwoCategories = getTopTwoCategories(userScoresLLI);
 
+                // Get the most common public category
                 var commonCategoryResponse = await readDataOnlyDAO.ReadData(sqlCommonPublic);
 
-                // temp
-                var topTwoKeys = userScoresLLI.OrderByDescending(kvp => kvp.Value)
-                                    .Take(2)
-                                    .Select(kvp => kvp.Key)
-                                    .ToList();
-                                    
+                // Construct the sql, based on the user's scores, the most common public category, and the number of recomendations requested byuser
+                var recomendtaionQuery = constructRecSql(numRecs);
 
-                var recomendtaionQuery = pullRecs(numRecs);
+                // Get the recomendations
                 var recomendationResponse = await readDataOnlyDAO.ReadData(recomendtaionQuery);
 
-                response.Output = cleanRecs(recomendationResponse);
+                // Clean the recomendations, removing info that could be used to identify users
+                var cleanedRecomendations = cleanRecs(recomendationResponse);
+                
+                // Populate response
+                response.Output = cleanedRecomendations;
 
+                // logger.Log(TODO: log the response here);
                 return response;
             }
             catch (Exception e)
@@ -67,48 +74,52 @@ namespace Peace.Lifelog.RE
         private Dictionary<string, int> scoreInit(Response formResponse)
         {
             Dictionary<string, int> userScores = new Dictionary<string, int>();
+            if (formResponse.Output == null) return userScores;
             foreach (List<Object> pair in formResponse.Output)
             {
-                if (pair[0] != null && pair[1] != null)
+                if (pair[0] == null || pair[1] == null)
                 {
-                    int rank = Convert.ToInt32(pair[1]);
-                    switch (rank)
-                    // Score categories using f1 scoring system
-                    {
-                        case 1:
-                            userScores.Add(pair[0].ToString(), 25);
-                            break;
-                        case 2:
-                            userScores.Add(pair[0].ToString(), 18);
-                            break;
-                        case 3:
-                            userScores.Add(pair[0].ToString(), 15);
-                            break;
-                        case 4:
-                            userScores.Add(pair[0].ToString(), 12);
-                            break;
-                        case 5:
-                            userScores.Add(pair[0].ToString(), 10);
-                            break;
-                        case 6:
-                            userScores.Add(pair[0].ToString(), 8);
-                            break;
-                        case 7:
-                            userScores.Add(pair[0].ToString(), 6);
-                            break;
-                        case 8:
-                            userScores.Add(pair[0].ToString(), 4);
-                            break;
-                        case 9:
-                            userScores.Add(pair[0].ToString(), 2);
-                            break;
-                        case 10:
-                            userScores.Add(pair[0].ToString(), 1);
-                            break;
-                        default:
-                            userScores.Add(pair[0].ToString(), 0);
-                            break;
-                    }
+                    return userScores;
+                }
+
+                string category = pair[0].ToString();
+                int rank = Convert.ToInt32(pair[1]);
+                switch (rank)
+                // Score categories using f1 scoring system
+                {
+                    case 1:
+                        userScores.Add(category, 25);
+                        break;
+                    case 2:
+                        userScores.Add(category, 18);
+                        break;
+                    case 3:
+                        userScores.Add(category, 15);
+                        break;
+                    case 4:
+                        userScores.Add(category, 12);
+                        break;
+                    case 5:
+                        userScores.Add(category, 10);
+                        break;
+                    case 6:
+                        userScores.Add(category, 8);
+                        break;
+                    case 7:
+                        userScores.Add(category, 6);
+                        break;
+                    case 8:
+                        userScores.Add(category, 4);
+                        break;
+                    case 9:
+                        userScores.Add(category, 2);
+                        break;
+                    case 10:
+                        userScores.Add(category, 1);
+                        break;
+                    default:
+                        userScores.Add(category, 0);
+                        break;
                 }
             }
             return userScores;
@@ -116,37 +127,48 @@ namespace Peace.Lifelog.RE
 
         private Dictionary<string, int> scoreLLI(Dictionary<string, int> scoreDict, Response lliResponse)
         {
-            foreach (List<Object>lli in lliResponse.Output)
+            if (lliResponse.Output != null)
             {
-                int statusMultiplier;  
-                switch (lli[1]) // index by whereever status is in the lli response
+                foreach (List<Object> lli in lliResponse.Output)
                 {
-                    case "Postponed":
-                        statusMultiplier = (int)0.8;
-                        break;
-                    case "Active":
-                        statusMultiplier = 1;
-                        break;
-                    case "Completed":
-                        statusMultiplier = (int)1.2;
-                        break;
-                    default:
-                        statusMultiplier = 1;
-                        break;
+                    int statusMultiplier;
+                    switch (lli[1]) // index by whereever status is in the lli response
+                    {
+                        case "Postponed":
+                            statusMultiplier = (int)0.8;
+                            break;
+                        case "Active":
+                            statusMultiplier = 1;
+                            break;
+                        case "Completed":
+                            statusMultiplier = (int)1.2;
+                            break;
+                        default:
+                            statusMultiplier = 1;
+                            break;
+                    }
+                    string? currentCategory = lli[0]?.ToString();
+                    if (currentCategory != null && scoreDict.ContainsKey(currentCategory)) // contains key category
+                    {
+                        scoreDict[currentCategory] += 5 * statusMultiplier;
+                    }
                 }
-                string currentCategory = lli[0].ToString();
-                if (scoreDict.ContainsKey(currentCategory)) // contains key category
-                {
-                    scoreDict[currentCategory] += 5 * statusMultiplier;
-                }
-                }
+            }
                 return scoreDict; 
             }
+
+        private List<string> getTopTwoCategories(Dictionary<string, int> scoreDict)
+        {
+            var topTwoKeys = scoreDict.OrderByDescending(kvp => kvp.Value)
+                                      .Take(2)
+                                      .Select(kvp => kvp.Key)
+                                      .ToList();
+            return topTwoKeys;
+        }
             
         // Responsible for dynamically constructing the SQL query to pull the records from the database
-        private string pullRecs(int numberRecomendations) // Params for helper need discussion
+        private string constructRecSql(int numberRecomendations) // Params for helper need discussion
         {
-            var recomendationResponse = new Response();
 
             // TODO : Add logic to pull records from the database
             string query = @"
@@ -198,63 +220,66 @@ namespace Peace.Lifelog.RE
 
         // Parse the records and clean them up
         // TODO : idetifiy personally identifiable info and "clean" it
-        private List<Object> cleanRecs(Response recomendationResposne)
+        private List<Object> cleanRecs(Response recomendationResponse)
         {
             List<object> cleanedRecs = new List<Object>();
-            foreach (List<Object> LLI in recomendationResposne.Output)
+            if (recomendationResponse.Output != null)
             {
-                int index = 0;
-                var lli = new LLI.LLI();
-                
-                foreach (var attribute in LLI) {
-                    if (attribute is null) continue;
+                foreach (List<Object> LLI in recomendationResponse.Output)
+                {
+                    int index = 0;
+                    var lli = new LLI.LLI();
                     
-                    switch(index){
-                        case 0:
-                            lli.LLIID = attribute.ToString() ?? "";
-                            break;
-                        case 1:
-                            lli.UserHash = attribute.ToString() ?? "";
-                            break;
-                        case 2:
-                            lli.Title = attribute.ToString() ?? "";
-                            break;
-                        case 3:
-                            lli.Category = attribute.ToString() ?? "";
-                            break;
-                        case 4:
-                            lli.Description = attribute.ToString() ?? "";
-                            break;
-                        case 5:
-                            lli.Status = attribute.ToString() ?? "";
-                            break;
-                        case 6:
-                            lli.Visibility = attribute.ToString() ?? "";
-                            break;
-                        case 7:
-                            lli.Deadline = attribute.ToString() ?? "";
-                            break;
-                        case 8:
-                            lli.Cost = Convert.ToInt32(attribute);
-                            break;
-                        case 9:
-                            lli.Recurrence.Status = attribute.ToString() ?? "";
-                            break;
-                        case 10:
-                            lli.Recurrence.Frequency = attribute.ToString() ?? "";
-                            break;
-                        case 11:
-                            lli.CreationDate = attribute.ToString() ?? "";
-                            break;
-                        case 12:
-                            lli.CompletionDate = attribute.ToString() ?? "";
-                            break;
-                        default:
-                            break;
+                    foreach (var attribute in LLI) {
+                        if (attribute is null) continue;
+                        
+                        switch(index){
+                            case 0:
+                                lli.LLIID = attribute.ToString() ?? "";
+                                break;
+                            case 1:
+                                lli.UserHash = attribute.ToString() ?? "";
+                                break;
+                            case 2:
+                                lli.Title = attribute.ToString() ?? "";
+                                break;
+                            case 3:
+                                lli.Category = attribute.ToString() ?? "";
+                                break;
+                            case 4:
+                                lli.Description = attribute.ToString() ?? "";
+                                break;
+                            case 5:
+                                lli.Status = attribute.ToString() ?? "";
+                                break;
+                            case 6:
+                                lli.Visibility = attribute.ToString() ?? "";
+                                break;
+                            case 7:
+                                lli.Deadline = attribute.ToString() ?? "";
+                                break;
+                            case 8:
+                                lli.Cost = Convert.ToInt32(attribute);
+                                break;
+                            case 9:
+                                lli.Recurrence.Status = attribute.ToString() ?? "";
+                                break;
+                            case 10:
+                                lli.Recurrence.Frequency = attribute.ToString() ?? "";
+                                break;
+                            case 11:
+                                lli.CreationDate = attribute.ToString() ?? "";
+                                break;
+                            case 12:
+                                lli.CompletionDate = attribute.ToString() ?? "";
+                                break;
+                            default:
+                                break;
+                        }
+                        index++;
                     }
-                    index++;
+                
                 }
-            
             }
         return cleanedRecs;
         }
