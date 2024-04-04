ca88c65c57260233cd65e47573d458729b50d883
y0shiki <yoshikiyarlagadda@gmail.com>
2024-04-03 17:09:36 -0700

    quick fix for codereview PN

--- src/frontend/PersonalNotePage/personalnote-dom-manipulation.js
+++ src/frontend/PersonalNotePage/personalnote-dom-manipulation.js
@@ -1,7 +1,7 @@
 export function setUp() {
     // Set today's date for note on bootup
     var date = new Date();
-    date.setDate(date.getDate());
+    date.setDate(date.getDate() - 1);
     document.getElementById('create-date-input').valueAsDate = date;
 
     //#region Done Button
