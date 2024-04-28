b059e490c7ed47169d4fe86aebabbfff4c07314b
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-04-27 18:24:39 -0700

    to change branch

--- src/frontend/cypress/e2e/MediaMementoSpec.cy.js
+++ src/frontend/cypress/e2e/MediaMementoSpec.cy.js
@@ -10,16 +10,24 @@ describe('Media Memento E2E Test', () => {
 
     // Act
     cy.get('div#93.lli').click();
-    cy.get('div.lli-media-container').click();
-    cy.wait(100000)
+    // cy.get('div.lli-media-container').click();
+    cy.wait(100)
 
     // Assert
+    cy.get('div.lli-media-container').should('contain', 'img');
   })
   it('View Media', () => {
     // Arrange
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/');
+    window.localStorage.clear()
+    window.localStorage.setItem('token-local', token)
+    // Visit the page
+    cy.wait(500)
 
     // Act
+    cy.get('div#93.lli').click();
+    // cy.get('div.lli-media-container').click();
+    cy.wait(500)
 
     // Assert
   })
