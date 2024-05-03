2c9ddf5809524d2ac8854bc4f57e2a30f6ec9eef
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-05-03 12:57:10 -0700

    User Managment repo started, view and Delete pii

--- src/backend/Lifelog/Peace.Lifelog.Infrastructure/UserManagmentRepo.cs
+++ src/backend/Lifelog/Peace.Lifelog.Infrastructure/UserManagmentRepo.cs
@@ -1,51 +1,77 @@
 ﻿namespace Peace.Lifelog.Infrastructure;
+using Peace.Lifelog.DataAccess;
+using Peace.Lifelog.Logging;
+using DomainModels;
+using System.Text.RegularExpressions;
 
-// public class UserManagmentRepo : IUserManagmentRepo
-// {
-//     private readonly ILogger<UserManagmentRepo> _logger;
-//     private readonly LifelogDbContext _context;
+public class UserManagmentRepo : IUserManagmentRepo
+{
+    private readonly IReadDataOnlyDAO readDataOnlyDAO;
+    private readonly IDeleteDataOnlyDAO deleteDataOnlyDAO;
+    private readonly ILogging logger;
 
-//     public UserManagmentRepo(ILogger<UserManagmentRepo> logger, LifelogDbContext context)
-//     {
-//         _logger = logger;
-//         _context = context;
-//     }
+    public UserManagmentRepo(IReadDataOnlyDAO readDataOnlyDAO, IDeleteDataOnlyDAO deleteDataOnlyDAO, ILogging logger)
+    {
+        this.readDataOnlyDAO = readDataOnlyDAO;
+        this.deleteDataOnlyDAO = deleteDataOnlyDAO;
+        this.logger = logger;
+    }
 
-//     public async Task<Response> DeletePersonalIdentifiableInformation(string userHash)
-//     {
-//         try
-//         {
-//             var user = await _context.Users.FirstOrDefaultAsync(x => x.UserHash == userHash);
-//             if (user == null)
-//             {
-//                 return new Response { Status = "Error", Message = "User not found" };
-//             }
-//             _context.Users.Remove(user);
-//             await _context.SaveChangesAsync();
-//             return new Response { Status = "Success", Message = "User deleted successfully" };
-//         }
-//         catch (Exception ex)
-//         {
-//             _logger.LogError(ex, "Error in DeletePersonalIdentifiableInformation");
-//             return new Response { Status = "Error", Message = "Error in DeletePersonalIdentifiableInformation" };
-//         }
-//     }
+    public async Task<Response> DeletePersonalIdentifiableInformation(string userHash)
+    {
+        Response response = new Response();
+        try
+        {
+            // check for sql injection in userhash input
+            if(ContainsSQLInjection(userHash))
+            {
+                throw new Exception("SQL Injection detected in delete PII userhash");
+            }
 
-//     public async Task<Response> ViewPersonalIdentifiableInformation(string userHash)
-//     {
-//         try
-//         {
-//             var user = await _context.Users.FirstOrDefaultAsync(x => x.UserHash == userHash);
-//             if (user == null)
-//             {
-//                 return new Response { Status = "Error", Message = "User not found" };
-//             }
-//             return new Response { Status = "Success", Message = "User found", Data = user };
-//         }
-//         catch (Exception ex)
-//         {
-//             _logger.LogError(ex, "Error in ViewPersonalIdentifiableInformation");
-//             return new Response { Status = "Error", Message = "Error in ViewPersonalIdentifiableInformation" };
-//         }
-//     }
-// }
+            // substitute userhash in query
+            string deleteQuery = $"DELETE FROM LifelogDB.Logs WHERE UserHash = '{userHash}';";
+
+            // execute 
+            response = await deleteDataOnlyDAO.DeleteData(deleteQuery);
+        }
+        catch (Exception ex)
+        {
+            _ = await logger.CreateLog("logs", userHash, "Server", "Error", ex.Message);
+        }
+        return response;
+    }
+
+    public async Task<Response> ViewPersonalIdentifiableInformation(string userHash)
+    {
+        Response response = new Response();
+        try
+        {
+            // check for sql injection in userhash input
+            if(ContainsSQLInjection(userHash))
+            {
+                throw new Exception("SQL Injection detected in View PII userhash");
+            }
+
+            // substitute userhash in query
+            string selectQuery = $"SELECT * FROM LifelogDB.Logs WHERE UserHash = '{userHash}';";
+
+            // execute 
+            response = await readDataOnlyDAO.ReadData(selectQuery, null);
+        }
+        catch (Exception ex)
+        {
+            _ = await logger.CreateLog("logs", userHash, "Server", "Error", ex.Message);
+        }
+        return response;
+    }
+
+    private static bool ContainsSQLInjection(string input)
+    {
+        // Regular expression to check for sql injection
+        string pattern = @"('|;|--|\b(ALTER|CREATE|DELETE|DROP|EXEC|EXECUTE|INSERT|MERGE|SELECT|UPDATE|UNION|ALTER|GRANT|REVOKE)\b)";
+        Regex sqlCheckRegex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        // Check if the input string matches the SQL injection pattern
+        return sqlCheckRegex.IsMatch(input);
+    }
+}
---
+++ src/backend/Lifelog/Peace.Lifelog.InfrastructureTest/UserManagmentRepoShould.cs
@@ -0,0 +1,54 @@
+namespace Peace.Lifelog.InfrastructureTest;
+using DomainModels;
+using Peace.Lifelog.DataAccess;
+using Peace.Lifelog.Infrastructure;
+using Peace.Lifelog.Logging;
+using Xunit;
+using ZstdSharp.Unsafe;
+
+public class UserManagmentRepoShould
+{
+    [Fact]
+    public async Task DeletePersonalIdentifiableInformation()
+    {
+        // Arrange
+        var userHash = "userHash";
+        ICreateDataOnlyDAO createDataOnlyDAO = new CreateDataOnlyDAO();
+        IReadDataOnlyDAO readDataOnlyDAO = new ReadDataOnlyDAO();
+        IDeleteDataOnlyDAO deleteDataOnlyDAO = new DeleteDataOnlyDAO();
+        ILogTarget logTarget = new LogTarget(createDataOnlyDAO, readDataOnlyDAO);
+        ILogging logger = new Logging(logTarget);
+        var userManagmentRepo = new UserManagmentRepo(readDataOnlyDAO, deleteDataOnlyDAO, logger);
+
+        _ = await logger.CreateLog("logs", userHash, "Info", "Error", "message");
+
+        // Act
+        var response = await userManagmentRepo.DeletePersonalIdentifiableInformation(userHash);
+
+        // Assert
+        Assert.True(response.HasError == false);
+    }
+
+    [Fact]
+    public async Task ViewPersonalIdentifiableInformation()
+    {
+        // Arrange
+        // TODO: Replace with a test user
+        var userHash = "System";
+        ICreateDataOnlyDAO createDataOnlyDAO = new CreateDataOnlyDAO();
+        IReadDataOnlyDAO readDataOnlyDAO = new ReadDataOnlyDAO();
+        IDeleteDataOnlyDAO deleteDataOnlyDAO = new DeleteDataOnlyDAO();
+        ILogTarget logTarget = new LogTarget(createDataOnlyDAO, readDataOnlyDAO);
+        ILogging logger = new Logging(logTarget);
+        var userManagmentRepo = new UserManagmentRepo(readDataOnlyDAO, deleteDataOnlyDAO, logger);
+
+        _ = await logger.CreateLog("Logs", userHash, "Info", "View", "view PII test");
+        _ = await logger.CreateLog("Logs", userHash, "Info", "View", "view PII test");
+
+        // Act
+        var response = await userManagmentRepo.ViewPersonalIdentifiableInformation(userHash);
+
+        // Assert
+        Assert.True(response.HasError == false);
+    }
+}
