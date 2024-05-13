e97648a696cef38f88b148623330f4667788ff18
Jack Pickle <80733460+jackpickle@users.noreply.github.com>
2024-05-12 21:58:26 -0700

    try catch lli controller

--- src/backend/Lifelog/Peace.Lifelog.LLIWebService/Controllers/LLIController.cs
+++ src/backend/Lifelog/Peace.Lifelog.LLIWebService/Controllers/LLIController.cs
@@ -39,103 +39,118 @@ public class LLIController : ControllerBase
     [Route("postLLI")]
     public async Task<IActionResult> PostLLI([FromBody] PostLLIRequest createLLIRequest)
     {
-        if (Request.Headers == null)
+        try
         {
-            return StatusCode(401);
+            if (Request.Headers == null)
+            {
+                return StatusCode(401);
+            }
+
+            var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
+
+            if (jwtToken == null)
+            {
+                return StatusCode(401);
+            }
+
+            var userHash = jwtToken.Payload.UserHash;
+
+            if (userHash == null)
+            {
+                return StatusCode(401);
+            }
+
+            if (!jwtService.IsJwtValid(jwtToken))
+            {
+                return StatusCode(401);
+            }
+
+            var lli = new LLI();
+            lli.Title = createLLIRequest.Title;
+            lli.Category1 = createLLIRequest.Category1;
+            lli.Category2 = createLLIRequest.Category2;
+            lli.Category3 = createLLIRequest.Category3;
+            lli.Description = createLLIRequest.Description;
+            lli.Status = createLLIRequest.Status;
+            lli.Visibility = createLLIRequest.Visibility;
+            lli.Deadline = createLLIRequest.Deadline;
+            lli.Cost = createLLIRequest.Cost;
+            lli.Recurrence.Status = createLLIRequest.RecurrenceStatus;
+            lli.Recurrence.Frequency = createLLIRequest.RecurrenceFrequency;
+
+            var response = await this.lliService.CreateLLI(userHash, lli);
+
+            if (response.HasError == false)
+            {
+                return Ok(response);
+            }
+            else if (response.ErrorMessage!.Contains("invalid") || response.ErrorMessage!.Contains("completed within the last year"))
+            {
+                return StatusCode(400, response.ErrorMessage);
+            }
+            else
+            {
+                return StatusCode(500, response.ErrorMessage);
+            }
+
         }
-
-        var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
-
-        if (jwtToken == null)
+        catch (Exception error)
         {
-            return StatusCode(401);
+            return StatusCode(500, error);
         }
-
-        var userHash = jwtToken.Payload.UserHash;
-
-        if (userHash == null)
-        {
-            return StatusCode(401);
-        }
-
-        if (!jwtService.IsJwtValid(jwtToken))
-        {
-            return StatusCode(401);
-        }
-
-        var lli = new LLI();
-        lli.Title = createLLIRequest.Title;
-        lli.Category1 = createLLIRequest.Category1;
-        lli.Category2 = createLLIRequest.Category2;
-        lli.Category3 = createLLIRequest.Category3;
-        lli.Description = createLLIRequest.Description;
-        lli.Status = createLLIRequest.Status;
-        lli.Visibility = createLLIRequest.Visibility;
-        lli.Deadline = createLLIRequest.Deadline;
-        lli.Cost = createLLIRequest.Cost;
-        lli.Recurrence.Status = createLLIRequest.RecurrenceStatus;
-        lli.Recurrence.Frequency = createLLIRequest.RecurrenceFrequency;
-
-        var response = await this.lliService.CreateLLI(userHash, lli);
-
-        if (response.HasError == false)
-        {
-            return Ok(response);
-        }
-        else if (response.ErrorMessage!.Contains("invalid") || response.ErrorMessage!.Contains("completed within the last year"))
-        {
-            Console.WriteLine(response.ErrorMessage!);
-            return StatusCode(400, response.ErrorMessage);
-        }
-        else
-        {
-            return StatusCode(500, response.ErrorMessage);
-        }
-
+        
     }
 
     [HttpGet]
     [Route("getAllLLI")]
     public async Task<IActionResult> GetAllLLI()
     {
-        if (Request.Headers == null)
+        try
         {
-            return StatusCode(401);
+            if (Request.Headers == null)
+            {
+                return StatusCode(401);
+            }
+
+            var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
+
+            if (jwtToken == null)
+            {
+                return StatusCode(401);
+            }
+
+            var userHash = jwtToken.Payload.UserHash;
+
+            if (userHash == null)
+            {
+                return StatusCode(401);
+            }
+
+            if (!jwtService.IsJwtValid(jwtToken))
+            {
+                return StatusCode(401);
+            }
+
+            var response = await this.lliService.GetAllLLIFromUser(userHash);
+
+            if (response.HasError == false)
+            {
+                return Ok(response);
+            }
+            else if (response.ErrorMessage!.Contains("UserHash can not be empty"))
+            {
+                return StatusCode(400, response.ErrorMessage);
+            }
+            else
+            {
+                return StatusCode(500);
+            }
         }
-
-        var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
-
-        if (jwtToken == null)
+        catch (Exception error)
         {
-            return StatusCode(401);
+            return StatusCode(500, error);
         }
-
-        var userHash = jwtToken.Payload.UserHash;
-
-        if (userHash == null)
-        {
-            return StatusCode(401);
-        }
-
-        if (!jwtService.IsJwtValid(jwtToken))
-        {
-            return StatusCode(401);
-        }
-
-        var response = await this.lliService.GetAllLLIFromUser(userHash);
-
-        if (response.HasError == false)
-        {
-            return Ok(response);
-        }
-        else if (response.ErrorMessage!.Contains("UserHash can not be empty"))
-        {
-            return StatusCode(400, response.ErrorMessage);
-        }
-        else
-        {
-            return StatusCode(500);
-        }
+        
     }
 
     [HttpGet]
@@ -211,104 +226,119 @@ public class LLIController : ControllerBase
     [Route("putLLI")]
     public async Task<IActionResult> PutLLI([FromBody] PutLLIRequest updateLLIRequest)
     {
-        if (Request.Headers == null)
+        try
         {
-            return StatusCode(401);
+            if (Request.Headers == null)
+            {
+                return StatusCode(401);
+            }
+
+            var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
+
+            if (jwtToken == null)
+            {
+                return StatusCode(401);
+            }
+
+            var userHash = jwtToken.Payload.UserHash;
+
+            if (userHash == null)
+            {
+                return StatusCode(401);
+            }
+
+            if (!jwtService.IsJwtValid(jwtToken))
+            {
+                return StatusCode(401);
+            }
+
+            var lli = new LLI();
+            lli.LLIID = updateLLIRequest.LLIID;
+            lli.Title = updateLLIRequest.Title;
+            lli.Category1 = updateLLIRequest.Category1;
+            lli.Category2 = updateLLIRequest.Category2;
+            lli.Category3 = updateLLIRequest.Category3;
+            lli.Description = updateLLIRequest.Description;
+            lli.Status = updateLLIRequest.Status;
+            lli.Visibility = updateLLIRequest.Visibility;
+            lli.Deadline = updateLLIRequest.Deadline;
+            lli.Cost = updateLLIRequest.Cost;
+            lli.Recurrence.Status = updateLLIRequest.RecurrenceStatus;
+            lli.Recurrence.Frequency = updateLLIRequest.RecurrenceFrequency;
+
+            var response = await this.lliService.UpdateLLI(userHash, lli);
+
+            if (response.HasError == false)
+            {
+                return Ok(response);
+            }
+            else if (response.ErrorMessage!.Contains("invalid") || response.ErrorMessage!.Contains("completed within the last year"))
+            {
+                return StatusCode(400, response.ErrorMessage);
+            }
+            else
+            {
+                return StatusCode(500);
+            }
         }
-
-        var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
-
-        if (jwtToken == null)
+        catch (Exception error)
         {
-            return StatusCode(401);
+            return StatusCode(500, error);
         }
-
-        var userHash = jwtToken.Payload.UserHash;
-
-        if (userHash == null)
-        {
-            return StatusCode(401);
-        }
-
-        if (!jwtService.IsJwtValid(jwtToken))
-        {
-            return StatusCode(401);
-        }
-
-        var lli = new LLI();
-        lli.LLIID = updateLLIRequest.LLIID;
-        lli.Title = updateLLIRequest.Title;
-        lli.Category1 = updateLLIRequest.Category1;
-        lli.Category2 = updateLLIRequest.Category2;
-        lli.Category3 = updateLLIRequest.Category3;
-        lli.Description = updateLLIRequest.Description;
-        lli.Status = updateLLIRequest.Status;
-        lli.Visibility = updateLLIRequest.Visibility;
-        lli.Deadline = updateLLIRequest.Deadline;
-        lli.Cost = updateLLIRequest.Cost;
-        lli.Recurrence.Status = updateLLIRequest.RecurrenceStatus;
-        lli.Recurrence.Frequency = updateLLIRequest.RecurrenceFrequency;
-
-        var response = await this.lliService.UpdateLLI(userHash, lli);
-
-        if (response.HasError == false)
-        {
-            return Ok(response);
-        }
-        else if (response.ErrorMessage!.Contains("invalid") || response.ErrorMessage!.Contains("completed within the last year"))
-        {
-            return StatusCode(400, response.ErrorMessage);
-        }
-        else
-        {
-            return StatusCode(500);
-        }
+        
     }
 
     [HttpDelete]
     [Route("deleteLLI")]
     public async Task<IActionResult> DeleteLLI(string lliId)
     {
-        if (Request.Headers == null)
+        try
         {
-            return StatusCode(401);
+            if (Request.Headers == null)
+            {
+                return StatusCode(401);
+            }
+
+            var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
+
+            if (jwtToken == null)
+            {
+                return StatusCode(401);
+            }
+
+            var userHash = jwtToken.Payload.UserHash;
+
+            if (userHash == null)
+            {
+                return StatusCode(401);
+            }
+
+            if (!jwtService.IsJwtValid(jwtToken))
+            {
+                return StatusCode(401);
+            }
+
+            var lli = new LLI();
+            lli.LLIID = lliId;
+
+            var response = await this.lliService.DeleteLLI(userHash, lli);
+
+            if (response.HasError == false)
+            {
+                return Ok(response);
+            }
+            else if (response.ErrorMessage!.Contains("invalid"))
+            {
+                return StatusCode(400, response.ErrorMessage);
+            }
+            else
+            {
+                return StatusCode(500);
+            }
         }
-
-        var jwtToken = JsonSerializer.Deserialize<Jwt>(Request.Headers["Token"]!);
-
-        if (jwtToken == null)
+        catch (Exception error)
         {
-            return StatusCode(401);
-        }
-
-        var userHash = jwtToken.Payload.UserHash;
-
-        if (userHash == null)
-        {
-            return StatusCode(401);
-        }
-
-        if (!jwtService.IsJwtValid(jwtToken))
-        {
-            return StatusCode(401);
-        }
-
-        var lli = new LLI();
-        lli.LLIID = lliId;
-
-        var response = await this.lliService.DeleteLLI(userHash, lli);
-
-        if (response.HasError == false)
-        {
-            return Ok(response);
-        }
-        else if (response.ErrorMessage!.Contains("invalid"))
-        {
-            return StatusCode(400, response.ErrorMessage);
-        }
-        else
-        {
-            return StatusCode(500);
+            return StatusCode(500, error);
         }
     }
 
